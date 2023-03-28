3e6a2345d0b7601fea16d941e0db7f1accba8b9a
behailuG <barch.mami@gmail.com>
2023-03-28 11:07:38 -0700

    more format

---
+++ more_formats.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_int_binary - integer binary
+ * @args: argument
+ * Return: binary
+ */
+int print_int_binary(va_list args)
+{
+	unsigned int x = 0;
+	int b = 0, new = 0;
+
+	new = va_arg(args, int);
+	x = new;
+	if (new < 0)
+	{
+		writetostdout('1');
+		new = new * -1;
+		x = new;
+		b += 1;
+	}
+	while (x > 0)
+	{
+		x = x / 2;
+		b++;
+	}
+	recursion_int_binary(new);
+	return (b);
+}
+
+/**
+  * recursion_int_binary - Prints a binary
+  * @a: integer to print
+  *
+  */
+void recursion_int_binary(int a)
+{
+	unsigned int t;
+
+	t = a;
+	if (t / 2)
+		recursion_int_binary(t / 2);
+	writetostdout(t % 2 + '0');
+}
