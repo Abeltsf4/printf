82fb78eadf9df203c60668d7a89bb24949177789
behailuG <barch.mami@gmail.com>
2023-03-29 09:10:26 -0700

    octal

--- _unsigned_functions.c
+++ _unsigned_functions.c
@@ -7,15 +7,21 @@
 int print_octal(va_list args)
 {
 	unsigned int x = 0;
-	int i = 0;
+	int i = 0, j = 0;
 
-	x = va_arg(args, unsigned int);
-	while (x > 0)
+	x = va_arg(args, int);
+	j = x;
+	if (j < 0)
 	{
-		x /= 8;
+		j *= -1;
+		x = j;
+	}
+	while (j > 0)
+	{
+		j /= 8;
 		i++;
 	}
-	recursion_octa(x);
+	recursion_octal(x);
 	return (i);
 }
 /**
@@ -26,5 +32,5 @@ void recursion_octal(unsigned int x)
 {
 	if (x / 8)
 		recursion_octal(x / 8);
-	writetostdout(x % 2 + '0');
+	writetostdout(x % 8 + '0');
 }
